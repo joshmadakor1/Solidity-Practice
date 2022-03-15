1fa6d116a218ab117634fffd1c8d7f336a97b39e
Josh Madakor <josh.madakor@gmail.com>
2022-03-15 12:31:39 -0700

    Modified People Contract

--- People.sol
+++ People.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.6.0;
 
-contract People {
+contract SimpleStorage {
     // This will get initialized to 0!
     uint256 favoriteNumber;
 
@@ -11,6 +11,7 @@ contract People {
     }
 
     People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
@@ -21,7 +22,12 @@ contract People {
     }
     
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+        //people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
+    }
+
+    function peopleCount() public view returns (uint256) {
+        return people.length;
     }
 
     
